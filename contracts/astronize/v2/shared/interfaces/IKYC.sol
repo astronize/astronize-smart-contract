14505c7479c93e69d7adf89a373859202f9705da
Jirapong Pangbud <jirapongpangbud@Jirapongs-MacBook-Pro.local>
2024-02-18 15:06:45 +0700

    re structure

--- contracts/astronize/v2/shared/interfaces/IKYC.sol
+++
@@ -1,5 +0,0 @@
-pragma solidity >=0.6.0;
-
-interface IKYC {
-  function kycsLevel(address _addr) external view returns (uint256);
-}
--- contracts/astronize/v2/shared/interfaces/INFTResaleHandler.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
-
-interface INFTResaleHandler {
-    function setSold(address _tokenAddress, uint256 _tokenId) external;
-    function canSell(address _tokenAddress, uint256 _tokenId) external view returns (bool);
-}
-

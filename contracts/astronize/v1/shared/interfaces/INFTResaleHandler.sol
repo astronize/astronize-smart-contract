03a54c53dba338fc9ceef19c27c51f9466910cde
Jirapong Pangbud <jirapongpangbud@Jirapongs-MacBook-Pro.local>
2024-02-18 14:45:59 +0700

    resale handler contract

---
+++ contracts/astronize/v1/shared/interfaces/INFTResaleHandler.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+interface INFTResaleHandler {
+    function setSold(address _tokenAddress, uint256 _tokenId) external;
+    function canSell(address _tokenAddress, uint256 _tokenId) external view returns (bool);
+}
+
